353c65fad878eaaae00787d952bdb5978a9aebf5
Leigh Shayler <leigh.shayler@gmail.com>
2016-11-13 21:08:08 +0800

    Updated the code to fetch the current power output from the inverter

--- solar.ino
+++ solar.ino
@@ -29,6 +29,9 @@
 #define USE_SERIAL Serial
 
 ESP8266WiFiMulti WiFiMulti;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
 
 char* _ssid = "test";
 uint _dummyUsage = 3300;
@@ -74,13 +77,15 @@ void setupClock() {
   oled.putString("Setting clock");
   //todo: get the time here
   setProgress(1);
+  timeClient.begin();
+  timeClient.update();
   delay(1000);
   oled.setTextXY(2,0);              // Set cursor position, start of line 0
   oled.putString("Clock is good");
   delay(1000);
 }
 
-void UpdateScreen(uint usage, char* timestamp, char* ssid, uint lastPushMins) {
+void UpdateScreen(double usage, String timestamp, char* ssid, uint lastPushMins) {
   oled.setTextXY(0,0);
   oled.putString("Running");
   oled.setTextXY(2,0);
@@ -102,6 +107,7 @@ void UpdateScreen(uint usage, char* timestamp, char* ssid, uint lastPushMins) {
 
 void setup()
 {
+  USE_SERIAL.begin(115200);
   Wire.begin();  
   oled.init();                      // Initialze SSD1306 OLED display
   oled.clearDisplay();              // Clear screen
@@ -114,9 +120,45 @@ void setup()
   oled.clearDisplay();              // Clear screen
 }
 
-uint getUsageValue() {
-  _dummyUsage++;
-  return _dummyUsage;
+double getUsageValue() {
+  //_dummyUsage++;
+  //return _dummyUsage;
+
+  HTTPClient http;
+  http.begin("http://10.1.1.14/pvi?rName=AcPower"); //HTTP
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if(httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+      USE_SERIAL.println(HTTP_CODE_OK);
+
+      // file found at server
+      if(httpCode == HTTP_CODE_OK) {
+          String payload = http.getString();
+          USE_SERIAL.println(payload);
+          //{"powerL1":0.0089695,"powerL2":0.0000000,"powerL3":0.0000000,"target":0,"status":1}
+          StaticJsonBuffer<200> jsonBuffer;
+          JsonObject& root = jsonBuffer.parseObject(payload);
+
+  
+          // Test if parsing succeeds.
+          if (!root.success()) {
+            USE_SERIAL.println("There was an error parsing JSON");
+            return -1;
+          }
+          else {
+            double powerL1 = root["powerL1"];
+            return powerL1;
+          }          
+      }
+  } 
+  else {
+      return -1;
+  }
+
+  http.end();
 }
 
 
@@ -124,8 +166,8 @@ uint getUsageValue() {
 void loop()
 {
   //todo: only do the processing if the wireless is still good, else reconnect
-  uint usage = getUsageValue();
-  char* timestamp = "10-10-10 12:12";
+  double usage = getUsageValue();
+  String timestamp = timeClient.getFormattedTime();
   UpdateScreen(usage, timestamp, _ssid, 5);
   delay(5*1000);
 }
